d7e4e75d12f68819c7539ea58d46f5e27119a838
Rosa Narváez <90695373+RosaNarMu@users.noreply.github.com>
2023-07-28 20:48:49 +0200

    dummy commit

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,13 @@
+using System.Numerics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public float speed;
+
+    public Vector3 movementDirection;
+
+
+}
