d639710a7c5bcd7029a1655f14c3b3e432a05990
Laurentino Vilchez <laurentino.vilchez@gmail.com>
2023-07-28 22:38:36 +0200

    movimiento basico

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,8 +5,13 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    public Vector3 Velocity;
+    public float MaxSpeed;
+    public float Acceleration;
+    public float Deceleration;
 
+    Vector3 Velocity;
+
+    Vector2 InputDir;
 
     void Start()
     {
@@ -14,10 +19,19 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        if (InputDir != Vector2.zero && Velocity.magnitude <= MaxSpeed)
+            Velocity = Vector3.MoveTowards(Velocity, new Vector3(MaxSpeed * InputDir.x, 0, MaxSpeed * InputDir.y), Time.deltaTime * Acceleration);
+
+        if (InputDir.x == 0)
+            Velocity.x = Mathf.MoveTowards(Velocity.x, 0, Time.deltaTime * Deceleration);
+        if (InputDir.y == 0)
+            Velocity.z = Mathf.MoveTowards(Velocity.z, 0, Time.deltaTime * Deceleration);
+
+        transform.position += Velocity * Time.deltaTime;
     }
 
     void OnMove(InputValue value)
     {
-        print(value.Get<Vector2>());
+        InputDir = value.Get<Vector2>();
     }
 }
