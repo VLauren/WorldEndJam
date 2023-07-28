d0802c1f2d7c494f7830c86fbc4db04e93218980
Rosa Narváez <90695373+RosaNarMu@users.noreply.github.com>
2023-07-28 21:16:06 +0200

    obstacle tiene cositas

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -1,13 +1,35 @@
-using System.Numerics;
+//using System.Numerics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    public float speed;
+    public float speed = 2;
 
-    public Vector3 movementDirection;
+    Vector3 movementDirection = new Vector3();
 
+    void Start()
+    {
 
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            transform.position = new Vector3(transform.position.x + Time.deltaTime, transform.position.y, transform.position.z);
+        };
+
+        transform.position += new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0) * Time.deltaTime * speed;
+
+        transform.position += movementDirection * Time.deltaTime;
+
+        if (transform.position.y < 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+
+            movementDirection.y = 0;
+        }
+    }
 }
