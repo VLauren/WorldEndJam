238d655af6b782f0d44589e87a510b41dbc24e99
Rosa Narváez <90695373+RosaNarMu@users.noreply.github.com>
2023-07-28 23:33:00 +0200

    obstaculo se mueve palante

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -5,9 +5,7 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    public float speed = 2;
-
-    Vector3 movementDirection = new Vector3();
+    public Vector3 movementDirection;
 
     void Start()
     {
@@ -16,20 +14,6 @@ public class Obstacle : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetButton("Fire1"))
-        {
-            transform.position = new Vector3(transform.position.x + Time.deltaTime, transform.position.y, transform.position.z);
-        };
-
-        transform.position += new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0) * Time.deltaTime * speed;
-
         transform.position += movementDirection * Time.deltaTime;
-
-        if (transform.position.y < 3)
-        {
-            transform.position = new Vector3(transform.position.x, 3, transform.position.z);
-
-            movementDirection.y = 3;
-        }
     }
 }
