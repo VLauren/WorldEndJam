4dc1289c9e87b26a0764375ba5fa0a1fea8f84ea
Rosa Narváez <90695373+RosaNarMu@users.noreply.github.com>
2023-07-29 18:30:48 +0200

    lo del damage go brrrrr

--- Assets/Scripts/UIDanger.cs
+++ Assets/Scripts/UIDanger.cs
@@ -7,54 +7,68 @@ using UnityEngine.UI;
 
 public class UIDanger : MonoBehaviour
 {
-    Player[] PlayerArray = { };
-
-    public bool NotInit = true;
-
-    public Slider sliderDanger;
+    Player[] PlayerArray;
 
     GameObject[] UIArray = new GameObject[4];
+
+    public Slider sliderPlayer1;
+
+    public Slider sliderPlayer2;
+
+    public Slider sliderPlayer3;
+
+    public Slider sliderPlayer4;
     void Start()
     {
         UIArray[0] = GameObject.Find("J1");
         UIArray[0].SetActive(false);
+        sliderPlayer1 = UIArray[0].transform.GetChild(0).GetComponent<Slider>();
+
         UIArray[1] = GameObject.Find("J2");
         UIArray[1].SetActive(false);
+        sliderPlayer2 = UIArray[1].transform.GetChild(0).GetComponent<Slider>();
+
         UIArray[2] = GameObject.Find("J3");
         UIArray[2].SetActive(false);
+        sliderPlayer3 = UIArray[2].transform.GetChild(0).GetComponent<Slider>();
+
         UIArray[3] = GameObject.Find("J4");
         UIArray[3].SetActive(false);
+        sliderPlayer4 = UIArray[3].transform.GetChild(0).GetComponent<Slider>();
 
     }
 
     void Update()
     {
+        print("JOINING" + Game.Instance.Joining);
+
         if (Game.Instance.Joining)
         {
-            NotInit = false;
             PlayerArray = FindObjectsOfType<Player>();
-
-            print(PlayerArray.Length);
         }
 
         if (PlayerArray.Length >= 1)
         {
             UIArray[0].SetActive(true);
+            sliderPlayer1.value = PlayerArray[0].ReceivedDamage;
         }
 
         if (PlayerArray.Length == 2)
         {
             UIArray[1].SetActive(true);
+            sliderPlayer2.value = PlayerArray[1].ReceivedDamage;
         }
 
         if (PlayerArray.Length == 3)
         {
             UIArray[2].SetActive(true);
+            sliderPlayer3.value = PlayerArray[2].ReceivedDamage;
         }
 
         if (PlayerArray.Length == 4)
         {
             UIArray[3].SetActive(true);
+            sliderPlayer4.value = PlayerArray[3].ReceivedDamage;
         }
     }
 }
