412f6513ee0c3098e5dcc12a1c46c10378910807
Rosa Narváez <90695373+RosaNarMu@users.noreply.github.com>
2023-07-29 11:52:38 +0200

    obstaculos hacen cosas

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    public GameObject Obstacle01Prefab;
+
+    public float ObstacleSpawnRate = 0.3f;
+
     void Start()
     {
+        InvokeRepeating("SpawnObstacle", 2, 1f / ObstacleSpawnRate);
     }
 
     void Update()
     {
-        
+
     }
 
     private void OnDrawGizmos()
@@ -21,5 +26,16 @@ public class Spawner : MonoBehaviour
         Gizmos.DrawCube(Vector3.zero, Vector3.one);
         Gizmos.color = new Color(0.0f, 1.0f, 0.0f, 0.75f);
         Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+    }
+
+    void SpawnObstacle()
+    {
+        float RandomX = Random.Range((transform.localScale.x * -1) / 2, transform.localScale.x / 2);
+
+        float RandomZ = Random.Range((transform.localScale.z * -1) / 2, transform.localScale.z / 2);
+
+        Instantiate(Obstacle01Prefab, transform.position + new Vector3(RandomX, 0, RandomZ), Quaternion.identity);
+
+
     }
 }
