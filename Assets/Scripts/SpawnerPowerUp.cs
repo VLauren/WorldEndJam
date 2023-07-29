9b25ec8a88e4a15f85b69a2783edca3be242f7b2
Laurentino Vilchez <laurentino.vilchez@gmail.com>
2023-07-29 21:08:32 +0200

    cosas del spawn de powerup

--- Assets/Scripts/SpawnerPowerUp.cs
+++ Assets/Scripts/SpawnerPowerUp.cs
@@ -24,9 +24,9 @@ public class SpawnerPowerUp : MonoBehaviour
     {
         Gizmos.matrix = transform.localToWorldMatrix;
 
-        Gizmos.color = new Color(0.0f, 0.75f, 0.0f, 0.75f);
+        Gizmos.color = new Color(0.0f, 0.0f, 0.75f, 0.75f);
         Gizmos.DrawCube(Vector3.zero, Vector3.one);
-        Gizmos.color = new Color(0.0f, 1.0f, 0.0f, 0.75f);
+        Gizmos.color = new Color(0.0f, 0.0f, 1.0f, 0.75f);
         Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
     }
 
