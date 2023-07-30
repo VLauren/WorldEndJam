6af84b9683b5d46397b7e13164dd56869f038378
Laurentino Vilchez <laurentino.vilchez@gmail.com>
2023-07-30 12:45:23 +0200

    prefab menu music

---
+++ Assets/Scripts/MainMenuMusic.cs
@@ -0,0 +1,20 @@
+using KrillAudio.Krilloud;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuMusic : MonoBehaviour
+{
+    void Start()
+    {
+        KLAudioSource klas = GetComponent<KLAudioSource>();
+        klas.SetIntVar("musicvar", 0);
+        klas.Play("music");
+    }
+
+    void BeepSound()
+    {
+        Game.AudioSource.SetIntVar("ui_krillvar", 0);
+        Game.AudioSource.Play("ui_krill");
+    }
+}
